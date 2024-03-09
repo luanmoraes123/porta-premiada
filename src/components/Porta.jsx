0463620f75c323fc2b68dd56c8378224b744ee7f
luanmoraes123 <luan.moraees21@gmail.com>
2024-03-09 18:44:28 -0300

    adicionando porta

---
+++ src/components/Porta.jsx
@@ -0,0 +1,16 @@
+import styles from '../styles/Porta.module.css'
+
+const Porta = () => {
+  return (
+    
+    <div className={styles.area}>
+      <div className={styles.estrutura}>
+        <div className={styles.porta}>
+          <div className={styles.numero}>5</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Porta;
