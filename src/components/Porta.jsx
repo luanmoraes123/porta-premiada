ba255c3bafa5697c5410d18cb25707d90b8e7d7c
luanmoraes123 <luan.moraees21@gmail.com>
2024-03-09 19:06:04 -0300

    modificando porta

--- src/components/Porta.jsx
+++ src/components/Porta.jsx
@@ -1,14 +1,18 @@
 import styles from '../styles/Porta.module.css'
 
-const Porta = () => {
+const Porta = (props) => {
+
+  const selecionada = props.selecionada ? styles.selecionada : '';
   return (
     
     <div className={styles.area}>
-      <div className={styles.estrutura}>
+      <div className={` ${styles.estrutura} ${selecionada}`}>
         <div className={styles.porta}>
           <div className={styles.numero}>5</div>
+          <div className={styles.macaneta}></div>
         </div>
       </div>
+      <div className={styles.chao}></div>
     </div>
   )
 }
