073358b3309351646621767bd963ee0753d5b544
luanmoraes123 <luan.moraees21@gmail.com>
2024-03-12 09:40:50 -0300

    adicionando parametros de url para jogo

--- src/functions/portas.ts
+++ src/functions/portas.ts
@@ -1,9 +1,9 @@
 import PortaModel from '../model/PortaModel'
 
-export const criarPortas = (qtde: number, selecionada: number): PortaModel[] => {
+export const criarPortas = (qtde: number, comPresente: number): PortaModel[] => {
   return Array.from({ length: qtde }, (_, i) => {
     const numero = i + 1;
-    const temPresente = numero === selecionada
+    const temPresente = numero === comPresente
     return new PortaModel(numero, temPresente)
   })
 }
--- src/pages/jogo/[portas]/[temPresente].tsx
+++ src/pages/jogo/[portas]/[temPresente].tsx
@@ -1,11 +1,17 @@
-import Porta from '../components/Porta'
-import styles from '../styles/Jogo.module.css'
-import { useState } from 'react';
-import { atualizarPortas, criarPortas } from '../functions/portas';
+import Porta from '../../../components/Porta'
+import styles from '../../../styles/Jogo.module.css'
+import { useEffect, useState } from 'react';
+import { atualizarPortas, criarPortas } from '../../../functions/portas';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Jogo() {
-  const [portas, setPortas] = useState(criarPortas(10, 1))
+  const router = useRouter()
+  const [portas, setPortas] = useState([])
+
+  useEffect(() => {
+    setPortas(criarPortas(+router.query.portas, +router.query.temPresente))
+  }, [router.query])
 
   const renderizarPortas = () => {
     return portas.map(porta => <Porta key={porta.numero} value={porta}
