234fbb9bb5cae0fd55eedaf83498298ad559ded6
luanmoraes123 <luan.moraees21@gmail.com>
2024-03-12 12:02:57 -0300

    adicionando link para inciiar jogo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Cartao from '../components/Cartao'
 import styles from '../styles/home.module.css'
 
@@ -5,7 +6,20 @@ export default function Home() {
 
   return (
     <div className={styles.formulario}>
-      <Cartao />
+      <div>
+        <Cartao bgcolor='#c0392c'>
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao></Cartao>
+      </div>
+      <div>
+        <Cartao></Cartao>
+        <Cartao bgcolor='#28a085'>
+          <Link className={styles.link} href={`/jogo/4/3`}>
+            Iniciar
+          </Link>
+        </Cartao>
+      </div>
     </div>
   );
 }
